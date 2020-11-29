4e014f24f9b93bd6ca9b34e23f47f7ba506da508
MiguelAntonioMaciasMeza <32549138+MiguelAntonioMaciasMeza@users.noreply.github.com>
2020-11-28 20:46:01 -0800

    Update Database.swift

--- barky/Database/Database.swift
+++ barky/Database/Database.swift
@@ -1,20 +1,21 @@
-//
+
 //  Database.swift
-//  barky
+//  dog tinder
 //
-//  Created by Derrick Lee on 11/12/20.
+//  Created by Derrick Lee on 10/15/20.
 //
 
 import Foundation
 
 var placeholderDesc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis, urna a bibendum convallis, est diam convallis ante, sed luctus orci leo id nulla. Nunc eget turpis congue, tempor ex nec, consectetur odio."
-var pompom: Dog = Dog(image: "pompom", name: "Pebbles", breed: "Pomeranian", gender: "F", temperament: "Lazy", size: "7' ", weight: 5 , description: placeholderDesc)
-var husky: Dog = Dog(image: "husky", name: "Dot", breed: "Husky", gender: "M", temperament: "Active", size: "24' ", weight: 55 , description: placeholderDesc)
-var bulldog: Dog = Dog(image: "englishbulldog", name: "Bobbie", breed: "English Bulldog", gender: "M", temperament: "Shy", size: "13' ", weight: 50 , description: placeholderDesc)
-var goldie: Dog = Dog(image: "goldie", name: "Big Boy", breed: "Golden Retriever", gender: "F", temperament: "Active", size: "22' ", weight: 65 , description: placeholderDesc)
+var pompom: Dog = Dog(image: "pompom", name: "Pebbles", breed: .Pomeranian, gender: .female, temperament: "Lazy", size: .small, weight: "5" , description: placeholderDesc)
+var husky: Dog = Dog(image: "husky", name: "Dot", breed: .Husky, gender: .male, temperament: "Active", size: .large, weight: "55" , description: placeholderDesc)
+var bulldog: Dog = Dog(image: "englishbulldog", name: "Bobbie", breed: .EnglishBulldog, gender: .male, temperament: "Shy", size: .medium, weight: "50" , description: placeholderDesc)
+var goldie: Dog = Dog(image: "goldie", name: "Big Girl", breed: .GoldenRetriever, gender: .female, temperament: "Active", size: .large, weight: "65" , description: placeholderDesc)
 
 
-var user: User = User(firstName:"Miguel", lastName: "Lee", city: "Fontana", state: "California", email: "miguel@gmail.com", listOfDogs: [pompom, husky])
-var user2: User = User(firstName:"Blue", lastName: "Bayani", city: "Fremon", state: "California", email: "blueb@gmail.com", listOfDogs: [bulldog, goldie])
+var user: User = User(firstName:"Miguel", lastName: "Lee", city: "Fontana", state: "California", email: "miguel@gmail.com", morning: true, afterNoon: true, night: true, listOfDogs: [pompom, husky])
+var user2: User = User(firstName:"Blue", lastName: "Bayani", city: "Fremon", state: "California", email: "blueb@gmail.com", morning: true, afterNoon: true, night: true, listOfDogs: [bulldog, goldie])
 
 var userArr: [User] = [user,user2]
+
